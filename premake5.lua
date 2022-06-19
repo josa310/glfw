523057756f6bd4faaf93ce8c542ef9bcd07b2c37
Jozsef <jozsef.maronka@gmail.com>
2022-06-19 14:46:23 +0200

    premake fix

--- premake5.lua
+++ premake5.lua
@@ -3,19 +3,26 @@ project "GLFW"
 	language "C"
 	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	
 	files
 	{
-		"include/GLFW/glfw.h",
+		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
 	}
 	
 	filter "system.windows"
@@ -27,6 +34,7 @@ project "GLFW"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
